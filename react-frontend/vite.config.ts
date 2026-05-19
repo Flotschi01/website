a0be1ea71e850fb686a958af9db7f793a7aa4717
FlotschiL <f.lehmerhofer@htlkrems.at>
2026-05-20 01:30:56 +0200

    vite listening address fix

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  host: true, // <--- THIS IS THE FIX. It exposes Vite to the local network.
 })
